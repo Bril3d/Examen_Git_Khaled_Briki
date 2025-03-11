27bac00ffe8f126da8dad424503745ac438f88e1
Khaled Briki <Khaledtbk@gmail.com>
2025-03-11 09:33:14 +0100

    Merge branch1 to main

--- projet.py
+++ projet.py
@@ -4,6 +4,14 @@ def somme(T):
 		S+=t
 	return S
 Data = [1,3,5]
+
 Som = sum(Data)
 print("La somme est:",Som)
+
+if Data:
+	print("La somme est:", sum(Data))
+	print("Le min est:", min(Data))
+	print("Le Max est:", max(Data))
+else:
+	print("liste vide")
 
