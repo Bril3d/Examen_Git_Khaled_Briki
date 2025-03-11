da91c30c21682bdad22f548ffd7dd425cf5044f0
Khaled Briki <Khaledtbk@gmail.com>
2025-03-11 09:30:49 +0100

    ajout de gestion des listes vides

--- projet.py
+++ projet.py
@@ -1,5 +1,7 @@
 Data = [1,3,5]
-print("La somme est:", sum(Data))
-print("Le min est:", min(Data))
-print("Le Max est:", max(Data))
-
+if Data:
+	print("La somme est:", sum(Data))
+	print("Le min est:", min(Data))
+	print("Le Max est:", max(Data))
+else:
+	print("liste vide")
